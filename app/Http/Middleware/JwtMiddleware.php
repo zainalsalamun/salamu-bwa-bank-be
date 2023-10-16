ab9fe5e81674734320659df445d91e62e5f4a225
Zainal Salamun <zainalrtf@gmail.com>
2023-10-16 18:37:23 +0700

    add middleware

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Exception;
+use Tymon\JWTAuth\Contracts\Providers\JWT;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+
+class JwtMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $user =JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e){
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['message' => 'Token is Invalid'], 401);
+            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['message' => 'Token is Expired'], 401);
+            }else{
+                return response()->json(['message' => 'Authorization Token not found'], 401);
+            }
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use App\Http\Controllers\Api\AuthController;
 */
 
 Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route ::middleware('jwt.verify')->get('test', function (Request $request) {
+   return 'success';
+});
