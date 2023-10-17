9e1febe86ff16fabe9fbe3a4e979f986f5488b51
Zainal Salamun <zainalrtf@gmail.com>
2023-10-17 14:40:48 +0700

    add nulable table transaction

--- database/migrations/2023_10_09_142815_create_transactions_table.php
+++ database/migrations/2023_10_09_142815_create_transactions_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('transaction_type_id')->constrained('transaction_types');
             $table->foreignId('payment_method_id')->constrained('payment_methods');
-            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('product_id')->nullable()->constrained('products');
             $table->float('amount', 10, 2);
             $table->string('transaction_code') ;
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->string('status');
             $table->timestamps();
         });
