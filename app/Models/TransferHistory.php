ce91681ee1520a12132624fa76365beb7243a61e
Zainal Salamun <zainalrtf@gmail.com>
2023-10-18 22:22:46 +0700

    add function receiverUser

--- app/Models/TransferHistory.php
+++ app/Models/TransferHistory.php
@@ -16,4 +16,15 @@ class TransferHistory extends Model
         'receiver_id',
         'transaction_code',
     ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+        'transaction_code',
+    ];
+
+    public function receiverUser()
+    {
+        return $this->belongsTo(User::class, 'receiver_id','id');
+    }
 }
