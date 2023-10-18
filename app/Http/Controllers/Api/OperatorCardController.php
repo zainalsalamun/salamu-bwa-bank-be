9e75f67e6afe6643c01febd74bf1a34e166d53da
Zainal Salamun <zainalrtf@gmail.com>
2023-10-18 20:15:46 +0700

    add operatorCardController

---
+++ app/Http/Controllers/Api/OperatorCardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\OperatorCard;
+
+class OperatorCardController extends Controller
+{
+    public function index(Request $request)
+    {
+        $limit = $request->query('limit')? $request->query('limit') : 10;
+        $operatorCards = OperatorCard::with('dataPlans')
+            ->where('status', 'active')
+            ->paginate($limit);
+        //menambahkan thumnail url
+        $operatorCards->getCollection()->transform(function ($item) {
+            $item->thumbnail = $item->thumbnail ? url ($item->thumbnail) : "";
+            return $item;
+        });
+
+        return response()->json($operatorCards);
+    }
+}
