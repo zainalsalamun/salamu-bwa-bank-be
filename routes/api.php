bddb558cc0d1c0b77dbeca9f2f30cdf217436214
Zainal Salamun <zainalrtf@gmail.com>
2023-10-18 10:50:42 +0700

    add route webhook

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\topUpController;
+use App\http\Controllers\Api\WebhookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,8 @@ use App\Http\Controllers\Api\topUpController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::post('webhooks', [WebhookController::class, 'update']);
 
 Route::group(['middleware' => 'jwt.verify'], function ($router) {
    Route::post('top_ups', [topUpController::class, 'store']);
